95385c9c1102ac9f169fe928fab3384fe7439dd9
Kara Cavanaugh <karacav@msn.com>
2017-10-24 16:02:48 -0500

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import "./App.css";
 
 import Nav from "./Nav/Nav";
+import router from "../router";
 
-export function App( { children } ) {
-	return (
-		<div className="app">
-			<Nav />
-		</div>
-	);
+export function App({ children }) {
+  return (
+    <div className="app">
+      <Nav />
+      {router}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,32 +3,35 @@ import { connect } from "react-redux";
 
 import "./Nav.css";
 import javascriptLogo from "../../assets/javascript.svg";
+import { Link } from "react-router-dom";
 
-export function Nav( { cartTotal } ) {
-	return (
-		<nav className="nav">
-			<div className="nav__header-wrapper">
-					<img
-						alt="javascript logo"
-						className="nav__javascript-logo"
-						src={ javascriptLogo }
-					/>
-					<h3 className="nav__header">
-						The JavaScript Framework Shop
-					</h3>
-			</div>
-			<p className="nav__cart">Cart ( ${ /* total price of products in cart */ } )</p>
-		</nav>
-	);
+export function Nav({ cartTotal }) {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div className="nav__header-wrapper">
+          <img
+            alt="javascript logo"
+            className="nav__javascript-logo"
+            src={javascriptLogo}
+          />
+          <h3 className="nav__header">The JavaScript Framework Shop</h3>
+        </div>
+      </Link>
+      <Link to="/cart">
+        <p className="nav__cart">Cart ( ${cartTotal} )</p>
+      </Link>
+    </nav>
+  );
 }
 
-function mapStateToProps( { products, productsInCart } ) {
-	return {
-		cartTotal: products
-			.filter( product => productsInCart.includes( product.id ) )
-			.reduce( ( total, { price } ) => total + price, 0 )
-			.toFixed( 2 )
-	};
+function mapStateToProps({ products, productsInCart }) {
+  return {
+    cartTotal: products
+      .filter(product => productsInCart.includes(product.id))
+      .reduce((total, { price }) => total + price, 0)
+      .toFixed(2)
+  };
 }
 
-export default connect( mapStateToProps )( Nav );
+export default connect(mapStateToProps)(Nav);
